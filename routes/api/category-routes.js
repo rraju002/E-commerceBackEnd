3a76100d2cfe84c8333810f3a5f59c28cf479afe
Rhowen01 <rraju002@ucr.edu>
2022-11-12 17:39:45 -0800

    build update ID function

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,10 +36,37 @@ router.post('/', async(req, res)=> {
       res.status(404).json({ message:"Category already exists in the database"});
       return;
     };
+    //Create a new category
+    const categoryData = await Category.create(req.body);
+    //Send success message
+    res.status(200).json({ message: "Success!" });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-router.get('/:id', (req, res) => {
+
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(
+      req.params.id,
+      {
+        include: [
+          {
+            model: Product,
+          }
+        ]
+      }
+    );
+    if (!categoryData) {
+      res.status(404).json({ message: "No Results" });
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(404).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
